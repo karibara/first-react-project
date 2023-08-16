f1d2c0162a4c68c00cbb25c345c529e03aafe438
karibara <malgorzata.bozykowska@gmail.com>
2023-08-16 14:48:50 +0200

    Add component ExpenseDate nested in ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
-import  "./ExpenseItem.css";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-    
-    return (
-        <div className="expense-item">
-            <div>{props.date.toLocaleString("pl-PL", {weekday: "short", day:"numeric", month:"long", year:"numeric"})}</div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">
-                    PLN {props.amount}
-                </div>
-                
-            </div>
-        </div>
-    )
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">PLN {props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
