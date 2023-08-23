d4acea18da1d3a6d6e3751d2b7373bdb734254b2
karibara <malgorzata.bozykowska@gmail.com>
2023-08-23 17:02:13 +0200

    Add event listener to user input in ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,9 +4,17 @@ import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState("")
+    const [enteredAmount, setEnteredAmount] = useState("")
+    const [enteredDate, setEnteredDate] = useState("")
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
     }
 
     return(
@@ -19,12 +27,12 @@ const ExpenseForm = () => {
 
                 <div className="new-expense__control">
                     <label>Kwota</label>
-                    <input type="number" min="0.1" step="0.1" />
+                    <input type="number" min="0.1" step="0.1" onChange={amountChangeHandler}/>
                 </div>
 
                 <div className="new-expense__control">
                     <label>Data</label>
-                    <input type="date" min="2022-01-01" max="2025-12-31"/>
+                    <input type="date" min="2022-01-01" max="2025-12-31" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
