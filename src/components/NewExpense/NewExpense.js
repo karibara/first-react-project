e00db3b2a08ada1814f633031c3656a49a4356ca
malgorzata bozykowska <malgorzata.bozykowska@gmail.com>
2023-09-05 13:36:07 +0200

    Add props to pass enteredExpenseData to App

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,11 +2,23 @@ import "./NewExpense.css"
 
 import ExpenseForm from "./ExpenseForm"
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        // create new object, witch will store data from handler parameter
+        const expenseData = {
+            // destructuring object (parameter) and add it to expenseData object
+            ...enteredExpenseData,
+            // add new property
+            id: Math.random().toString()
+        }
+        
+        props.onAddExpense(expenseData);
+    }
+
 
     return (
         <div className="new-expense">
-            <ExpenseForm/>
+            <ExpenseForm onSaveData={saveExpenseDataHandler}/>
         </div>
     )
 }
